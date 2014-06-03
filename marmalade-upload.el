1e3275b5aa980e9f8d587d68ea8887157f998e74
Nic Ferrier <nferrier@ferrier.me.uk>
2014-06-04 00:23:14 +0100

    bump the version

--- marmalade-upload.el
+++ marmalade-upload.el
@@ -4,7 +4,7 @@
 
 ;; Author: Nic Ferrier <nferrier@ferrier.me.uk>
 ;; Keywords: lisp
-;; Version: 0.0.1
+;; Version: 0.0.2
 ;; Package-requires: ((web "0.4.1")(kv "0.0.19"))
 
 ;; This program is free software; you can redistribute it and/or modify
